55eb163d21d7f5bad74742de451e66895b4ad0a2
shu <shu@db>
2017-04-08 14:56:41 +0800

    旧文件移动到old文件夹，从新开始

---
+++ old/js/header.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > 60){
+            $("nav").css({"position":"fixed", "top":"0", "z-index":"2"});
+            $(".logo-header").css({"margin":"0 60px 0 0", "overflow":"hidden"});
+            $("nav a img").css({"margin":"-25px 0 0 80px"});
+            $("nav ul li a").hover(function(){
+                $(this).css("background", "#ddd");
+            }, function(){
+                $(this).css("background", "#fff");
+            });
+            $(".nav-last-link").css({"margin-right":"280px"});
+            $("#main").css({"margin-top":"105px"});
+        }
+        else{
+            $("nav").css({"position":"static"});
+            $(".logo-header").css({"margin":"0 60px 0 0", "overflow":"visible"});
+            $("nav a img").css({"margin":"-60px 0 0 120px"});
+            $("nav ul li a").hover(function(){
+                $(this).css("background", "#fff");
+            });
+            $(".nav-last-link").css({"margin-right":"195px"});
+            $("#main").css({"margin-top":"55px"});
+        }
+    });
+});
